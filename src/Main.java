1ec2c0cd419dae309dac6c46306de985a05788b6
dlaslavic <dejan.laslavic@gmail.com>
2024-10-10 21:02:53 +0200

    Zadatak6.1.While petlja

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,30 @@
 import java.util.Scanner;
 
+
+//Zadatak: Zbrajanje unesenih brojeva dok korisnik ne unese0
+//NApiši program koji koristi while petlju i traži od korisnika da uosi brojeve sve dok ne unese 0.
+//Program treba ispisati ukupni zbroj svih unesenih brojeva.
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int pokusaj;
-        int pogodak=7;
+        int broj;
+        int pogodak = 0;
+        int zbroj = 0;
 
-        do {
-            System.out.println("Unesite broj:");
-            pokusaj = scanner.nextInt();
-        }while (pokusaj != pogodak);
+        System.out.println("Unesi broj dok ne pogodis:");
+        broj = scanner.nextInt();
 
-        System.out.println("Pogodak!!!!");
+        while (broj!=pogodak) {
+            zbroj = zbroj + broj;
 
+            System.out.println("Pokusaj ponovo:");
+            broj = scanner.nextInt();
 
+        }
 
+        System.out.println("Rezultat je: " + zbroj);
     }
 
 }
