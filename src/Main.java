9984f7d8b53531c66dabe47f0dc3ba35a566a3a3
dlaslavic <dejan.laslavic@gmail.com>
2024-10-14 13:02:58 +0200

    Domaća zadaća_zadatak1.

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,27 @@
 import java.util.Scanner;
 
-//Tražiti od korisnika da odabere broj od 1 do 4 (svaki broj će označavati pojedinu
-// matematičku radnju: 1=zbrajanje, 2=oduzimanje, 3=množenje i 4=dijeljenje)
-//Ovisno o korisnikovom odabiru, potrebno je ispisati rezultat s pripadajućom
-//porukom koja je matematičkaradnja obavljena.
+//ZAdatak 1. NApišite program koji će izračunati sumu brojeva od 1 do N, gdje je N
+// pozitivan cijeli broj unesen od strane korisnika.
+// N=10
 
 public class Main {
 
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Odaberite broj od 1 do 4 za matematičku radnju:");
+        int parniBroj=0;
+        int neparniBroj=0;
 
-        System.out.println("1 = Zbrajanje");
-        System.out.println("2 = Oduzimanje");
-        System.out.println("3 = Množenje");
-        System.out.println("4 = Dijeljenje");
-
-        int odabirRadnje = scanner.nextInt();
-
-        System.out.println("Unesite prvi broj:");
-        double broj1 = scanner.nextDouble();
-        System.out.println("Unesite drugi broj:");
-        double broj2 = scanner.nextDouble();
-
-        if (odabirRadnje == 1) {
-            System.out.println("Rezultat zbrajanja je: " + (broj1 + broj2));
-        } else if (odabirRadnje == 2) {
-            System.out.println("Rezultat oduzimanja je: " + (broj1 - broj2));
-        } else if (odabirRadnje == 3) {
-            System.out.println("Rezultat množenja je: " + (broj1 * broj2));
-        } else if (odabirRadnje == 4) {
-            System.out.println("Rezultat dijeljenja je: " + (broj1 / broj2));
-        } else {
-            System.out.println("Nevažeći odabir. Pokušajte ponovno.");
+        for (int i = 1; i < 10; i++) {
+            if (i % 2 == 0){
+                parniBroj++;
+            }else{
+                neparniBroj++;
+            }
         }
+
+        System.out.println("Ukupni zbroj unesenih pozitivnih cijelih brojeva: "  + (parniBroj + neparniBroj));
 
     }
 
