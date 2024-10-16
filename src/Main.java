ffa6b1da4a43134677c3d725d2021dc8e9f328e2
dlaslavic <dejan.laslavic@gmail.com>
2024-10-16 20:50:29 +0200

    Zadatak_dodavanje i ispis parnih brojeva.

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,39 @@
 import java.util.Scanner;
 
+//Zadatak: Dodavanje i ispis parnih brojeva.
+//Korisnik unosi 10 brojeva, a program sprema samo parne brojeve u novo polje i zatim ih ispisuje.
+
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Unesite veličinu polja: ");
+        System.out.println("Unesite 10 brojeva: ");
 
-        int velicinaPolja = scanner.nextInt();
-        int[] polje = new int[velicinaPolja];
+        int[] uneseniBrojevi = new int[10];
+        int indexParnihBrojeva = 0;
+        int brojParnihBrojeva = 0;
 
-
-        for (int i = 0; i<polje.length; i++) {
-            System.out.println("Unesite vrijednost elemenata " + i + 1 + ". elementa:");
-            polje[i] = scanner.nextInt();
-
-        }
-
-        int maxElement = polje[0];
-        for (int i = 0; i < polje.length; i++) {
-            if (polje[i] > maxElement) {
-                maxElement = polje[i];
+        for (int i = 0; i < uneseniBrojevi.length; i++) {
+            System.out.println("Unesite " + (i + 1) + ". broj:");
+            uneseniBrojevi[i] = scanner.nextInt();
+            if (uneseniBrojevi[i] % 2 == 0){
+                brojParnihBrojeva++;
             }
         }
+        int[] parniBrojevi = new int[brojParnihBrojeva];
 
-        System.out.println("Maximalni element polja je: " + maxElement);
+        for (int i = 0; i < uneseniBrojevi.length; i++) {
+            if (uneseniBrojevi[i] % 2 == 0) {
+                parniBrojevi[indexParnihBrojeva] = uneseniBrojevi[i];
+                indexParnihBrojeva++;
+            }
+        }
+        for (int i = 0; i < parniBrojevi.length; i++) {
+            System.out.println("Rezultat je: " + parniBrojevi[i]);
+
+        }
 
     }
+
 }
