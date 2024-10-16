6559af6279df9d60421a152650d34d9503ab767c
dlaslavic <dejan.laslavic@gmail.com>
2024-10-16 18:38:49 +0200

    Rješenje zadaće s predavanja.

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int maxBroj = 10;
-        int[] uneseniBrojevi = new int[4];
+        System.out.println("Unesite veličinu polja: ");
 
-        System.out.println("Unesi 4 cijela broja: ");
-        System.out.println("Prvi broj: ");
+        int velicinaPolja = scanner.nextInt();
+        int[] polje = new int[velicinaPolja];
 
 
-        for (int i = 0; i <= uneseniBrojevi.length; i++) {
-            System.out.println(i + 1 + ". broj: ");
-            uneseniBrojevi[0] = scanner.nextInt();
+        for (int i = 0; i<polje.length; i++) {
+            System.out.println("Unesite vrijednost elemenata " + i + 1 + ". elementa:");
+            polje[i] = scanner.nextInt();
 
         }
-        for (int broj : uneseniBrojevi) {
-            if (broj > maxBroj) {
-                System.out.println("Broj koji je veći od 10: " + broj);
+
+        int maxElement = polje[0];
+        for (int i = 0; i < polje.length; i++) {
+            if (polje[i] > maxElement) {
+                maxElement = polje[i];
             }
         }
+
+        System.out.println("Maximalni element polja je: " + maxElement);
 
     }
 }
