b74993b7ff85e4e9092b982c6fa77a8e5037af03
dlaslavic <dejan.laslavic@gmail.com>
2024-10-17 19:36:34 +0200

    Provjera teleoperatera preko unesenog broja

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-//Od korisnika tražiti da unese 5 stringova te ispisati samo stringove koji se jednake duljine kao prvi string.
+//Provjeriti teleoperatera preko unesenog broja
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<String> lista = new ArrayList<>();
+        System.out.println("Unesite broj mobitela: ");
+        String phoneNumber = scanner.nextLine();
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Unesite vrijednost: ");
-            String unesenaVrijednost = scanner.nextLine();
-            lista.add(unesenaVrijednost);
+        Boolean a1 = phoneNumber.startsWith("091");
+        Boolean ht = phoneNumber.startsWith("098");
+
+        if (phoneNumber.startsWith("091")){
+            System.out.println("A1");
+        } else if (phoneNumber.startsWith("098")){
+            System.out.println("HT");
+        } else if (phoneNumber.startsWith("095")){
+            System.out.println("Telemach");
+        } else if (phoneNumber.startsWith("099")) {
+            System.out.println("Simpa");
+
         }
-
-        String prviElement = lista.get(0);
-        for (int i = 0; i < lista.size(); i++){
-            if (prviElement.length()==lista.get(i).length()) {
-                System.out.println(lista.get(i));
-
-            }
-
-       }
-
 
     }
 
