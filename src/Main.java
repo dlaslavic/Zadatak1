7a95bd1661a79fc880b500a373398d46c455eaed
dlaslavic <dejan.laslavic@gmail.com>
2024-10-17 19:09:45 +0200

    Primjer s jednakom duljinom stringa kao prvi string.

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,30 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-
+//Od korisnika tražiti da unese 5 stringova te ispisati samo stringove koji se jednake duljine kao prvi string.
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Unesite koliko elemenata zelite popuniti: ");
+        List<String> lista = new ArrayList<>();
 
-        int n = scanner.nextInt();
-
-        int[] staroPolje = new int[10];
-        List polje = new ArrayList<>();
-
-        var test = staroPolje.length;
-        var test1 = polje.size();
-
-        for (int i = 0; i < n; i++){
-            System.out.println("Unesite vrijednost" + (i + 1) + ". elementa: ");
-            int uneseniElement = scanner.nextInt();
-            polje.add(uneseniElement);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Unesite vrijednost: ");
+            String unesenaVrijednost = scanner.nextLine();
+            lista.add(unesenaVrijednost);
         }
 
-        for (int i = 0; i < polje.size(); i++){
-            System.out.println("Vrijednost elementa " + (i+ 1) + " je " + polje.get(i));
+        String prviElement = lista.get(0);
+        for (int i = 0; i < lista.size(); i++){
+            if (prviElement.length()==lista.get(i).length()) {
+                System.out.println(lista.get(i));
 
-        }
+            }
+
+       }
+
 
     }
 
