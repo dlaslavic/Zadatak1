f4f0f0adb0dc68d052971f09e854d503bf3bacfd
dlaslavic <dejan.laslavic@gmail.com>
2024-10-21 17:53:42 +0200

    Zadatak s brojanjem rijeci.

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //var test1 = !true;
-        //var test2 = !false;
-
         List<String> lista = new ArrayList<>();
         String uneseniString;
         Integer brojac = 0;
+        Integer brojRijeciJava = 0;
 
         do {
             System.out.println("Unesite string: ");
@@ -23,13 +21,22 @@ public class Main {
             lista.add(uneseniString);
         }while (!uneseniString.equals("Stop"));
 
-        for (int i = 0; i< lista.size(); i++){
-            if (lista.get(i).contains("Java"));{
+        for (int i= 0; i < lista.size(); i++){
+           String[] poljeRijeci = lista.get(i).split(" ");
+
+           for (int y = 0; y < poljeRijeci.length; y++){
+               if (poljeRijeci[y].equals("Java")){
+                   brojRijeciJava++;
+               }
+           }
+
+           if (lista.get(i).contains("Java"));{
                 brojac++;
-            }
+           }
         }
 
         System.out.println("Broj recenica koje sadrze Java rijec je: " + brojac);
+        System.out.println("Broj rijeci Java je: " + brojRijeciJava++);
     }
 
 }
