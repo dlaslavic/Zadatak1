2e834bb66dde8fc991ecebb941026829d644d2de
dlaslavic <dejan.laslavic@gmail.com>
2024-10-23 16:33:35 +0200

    Prvi zadatak.

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,44 @@
-import javax.swing.text.html.HTMLDocument;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-//Tražiti od korisnika unos liste stringova sve dok ne napiše "Stop". Nakon toga izbrojati koliko stringova sadrži
-//riječ "Java".
+//Zadatak1.Zamjena najvećeg i najmanjeg broja u List-i.
+//Napiši program koji od korisnika traži unos brojeva i sprema ih u Listu.
+//Pronađi najmanji i najveći broj, zamjeni ih međusobno i isši u novu listu.
+
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<String> lista = new ArrayList<>();
-        String uneseniString;
-        Integer brojac = 0;
-        Integer brojRijeciJava = 0;
+        System.out.println("Unesite koliko elemenata zelite popuniti: ");
 
-        do {
-            System.out.println("Unesite string: ");
-            uneseniString = scanner.nextLine();
-            lista.add(uneseniString);
-        }while (!uneseniString.equals("Stop"));
+        int n = scanner.nextInt();
+        List<Integer> polje = new ArrayList<>();
 
-        for (int i= 0; i < lista.size(); i++){
-           String[] poljeRijeci = lista.get(i).split(" ");
 
-           for (int y = 0; y < poljeRijeci.length; y++){
-               if (poljeRijeci[y].equals("Java")){
-                   brojRijeciJava++;
-               }
-           }
-
-           if (lista.get(i).contains("Java"));{
-                brojac++;
-           }
+        for (int i = 0; i < n; i++) {
+            System.out.println("Unesite vrijednost" + (i + 1) + ". elementa: ");
+            int uneseniElement = scanner.nextInt();
+            polje.add(uneseniElement);
         }
 
-        System.out.println("Broj recenica koje sadrze Java rijec je: " + brojac);
-        System.out.println("Broj rijeci Java je: " + brojRijeciJava++);
-    }
+        int minUneseniBroj = polje.get(0);
+        int maxUneseniBroj = polje.get(0);
 
+        for (int i = 0; i < polje.size(); i++) {
+            if (polje.get(i) < minUneseniBroj) {
+                minUneseniBroj = polje.get(i);
+            } else if (polje.get(i) > maxUneseniBroj) {
+                maxUneseniBroj = polje.get(i);
+            }
+        }
+        System.out.println("Maksimalna vrijednost elementa " + " je " + maxUneseniBroj);
+        System.out.println("Minimalna vrijednost elementa " + " je " + minUneseniBroj);
+
+    }
 }
+
+
+
