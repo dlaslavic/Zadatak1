8b2a361a028ca93cfd5d9a7684ae6db30a3bdf96
dlaslavic <dejan.laslavic@gmail.com>
2024-10-23 21:40:43 +0200

    Posljednji zadatak s predavanja

--- src/Main.java
+++ src/Main.java
@@ -3,40 +3,49 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-//Zadatak1.Zamjena najvećeg i najmanjeg broja u List-i.
-//Napiši program koji od korisnika traži unos brojeva i sprema ih u Listu.
-//Pronađi najmanji i najveći broj, zamjeni ih međusobno i isši u novu listu.
+//ZADATAK_Provjera da li lista sadrži duplikate
+//Tražiti od korisnika da unese broj elemenata u listi te popuni elemente liste.
+//Nakon toga, pretražiti po listi da li postoji ijedan duplikat, ukoliko postoji ijedan duplikat, ukoliko postoji
+// ispisi vrijednost true, ukoliko ne postoji ispisi vrijednot false.
+
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Unesite koliko elemenata zelite popuniti: ");
 
         int n = scanner.nextInt();
-        List<Integer> polje = new ArrayList<>();
+        List<Integer> brojevi = new ArrayList<>();
 
-
-        for (int i = 0; i < n; i++) {
-            System.out.println("Unesite vrijednost" + (i + 1) + ". elementa: ");
-            int uneseniElement = scanner.nextInt();
-            polje.add(uneseniElement);
+        System.out.println("Unesite brojeve:");
+        for (int i = 0; i < 10; i++) {
+            brojevi.add(scanner.nextInt());
         }
 
-        int minUneseniBroj = polje.get(0);
-        int maxUneseniBroj = polje.get(0);
+        Boolean duplikat = false;
 
-        for (int i = 0; i < polje.size(); i++) {
-            if (polje.get(i) < minUneseniBroj) {
-                minUneseniBroj = polje.get(i);
-            } else if (polje.get(i) > maxUneseniBroj) {
-                maxUneseniBroj = polje.get(i);
+        for (int i = 0; i < brojevi.size(); i++) {
+            int trenutniBroj = brojevi.get(i);
+
+            for (int j = i + 1; j < brojevi.size(); j++) {
+                if (i != j) {
+                    if (brojevi.get(j) == trenutniBroj) {
+                        System.out.println("True");
+                        duplikat = true;
+                        break;
+                    }
+                }
+
+                if (duplikat) {
+                    break;
+                }
+            }
+
+            if (!duplikat) {        //!duplikat = false
+                System.out.println("False");
             }
         }
-        System.out.println("Maksimalna vrijednost elementa " + " je " + maxUneseniBroj);
-        System.out.println("Minimalna vrijednost elementa " + " je " + minUneseniBroj);
-
     }
 }
 
