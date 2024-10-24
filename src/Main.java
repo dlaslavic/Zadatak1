22ebcbefec7501aa88bd06c57bcb3400cb8710ae
dlaslavic <dejan.laslavic@gmail.com>
2024-10-24 20:57:05 +0200

    Zadnji zadatak_

--- src/Main.java
+++ src/Main.java
@@ -4,36 +4,41 @@ import java.util.List;
 import java.util.Scanner;
 
 
+//Napraviti funkciju koja provjerava da li je lista uzlazno sortirana.
+//Prethodno listu popunjava korisnik svojim unosima.
+//Funkcija treba vratiti true ili false.
+//Na kraju programa korisniku ispisati da li je lista sortirana ili nije.
+
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
         List<Integer> listaBrojeva = new ArrayList<>();
 
-        for (int i = 1; i < 2; i++) {
+        for (int i = 1; i < 10; i++) {
             listaBrojeva.add(i);
         }
 
-        postojiLiParanBroj(listaBrojeva);
+        listaJeSortirana(listaBrojeva);
     }
 
-    public static void ispisiVrijednostElementa(Integer element) {
-        // Integer element;
-        System.out.println("Vrijednost je: " + element);
-    }
+    public static void listaJeSortirana(List<Integer> listaBrojeva) {
 
-    public static void postojiLiParanBroj(List<Integer> lista) {
+        Boolean rezultat = true;
 
-        for (Integer broj : lista) {
-            if (broj % 2 == 0) {
-                System.out.println("Paran!");
-                return;
+        for (int i = 1; i < listaBrojeva.size(); i++) {
+            if (listaBrojeva.get(i) > listaBrojeva.get(i)) {
+                System.out.println("Sortirana nije sortirana uzlazno.");
+                sortirano = false;
+                break;
             }
         }
-        System.out.println("Neparan!");
-
+        if (sortirano == true)
+            System.out.println("Nije sortirana!");
     }
+
+         return sortirano;
 
 }
 
