bd7b8819c2e50baf87ef0fc35246d5da88781948
dlaslavic <dejan.laslavic@gmail.com>
2024-10-24 19:21:48 +0200

    Oduzimanje_Funkcija

--- src/Main.java
+++ src/Main.java
@@ -3,49 +3,34 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-//ZADATAK_Provjera da li lista sadrži duplikate
-//Tražiti od korisnika da unese broj elemenata u listi te popuni elemente liste.
-//Nakon toga, pretražiti po listi da li postoji ijedan duplikat, ukoliko postoji ijedan duplikat, ukoliko postoji
-// ispisi vrijednost true, ukoliko ne postoji ispisi vrijednot false.
-
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Unesite koliko elemenata zelite popuniti: ");
+        System.out.println("Unesite prvi broj: ");
+        int prviBroj = scanner.nextInt();
+        ispisiVrijednostElementa(prviBroj);
 
-        int n = scanner.nextInt();
-        List<Integer> brojevi = new ArrayList<>();
+        System.out.println("Unesite drugi broj: ");
+        int drugiBroj = scanner.nextInt();
+        ispisiVrijednostElementa(drugiBroj);
 
-        System.out.println("Unesite brojeve:");
-        for (int i = 0; i < 10; i++) {
-            brojevi.add(scanner.nextInt());
+        Integer rezultatOduzimanja = oduzmiBrojeve(prviBroj, drugiBroj);
+
+        System.out.println("Rezultat je: " + rezultatOduzimanja);
         }
 
-        Boolean duplikat = false;
-
-        for (int i = 0; i < brojevi.size(); i++) {
-            int trenutniBroj = brojevi.get(i);
-
-            for (int j = i + 1; j < brojevi.size(); j++) {
-                if (i != j) {
-                    if (brojevi.get(j) == trenutniBroj) {
-                        System.out.println("True");
-                        duplikat = true;
-                        break;
-                    }
-                }
-
-                if (duplikat) {
-                    break;
-                }
-            }
-
-            if (!duplikat) {        //!duplikat = false
-                System.out.println("False");
-            }
+        public static void ispisiVrijednostElementa(Integer element){
+        // Integer element;
+        System.out.println("Vrijednost je: " + element);
         }
+
+        public static Integer oduzmiBrojeve(Integer element1, Integer element2){
+        Integer rezultat = element1 - element2;
+        return rezultat;
+
+
     }
 }
 
