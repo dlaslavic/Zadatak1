4d44abc7b943ee9e5877dd6684394180be0a2dcd
dlaslavic <dejan.laslavic@gmail.com>
2024-10-28 19:36:53 +0100

    Testni primjer stringa 1.2

--- src/Main.java
+++ src/Main.java
@@ -6,34 +6,37 @@ import java.util.List;
 import java.util.Scanner;
 
 
-//Telefonski imenik:
-// - Napravi program koji simulira telefonski imenik koristeći dvije paralelne liste
-// - Lista u koju se spremaju imena i lista u koju se spremaju brojevi
-// - Kod prvog odabira korisnik unosi ime i broj koji se spremaju u svoje liste
-// - Korisnik ima mogućnost odabira te za svaki odabir treba omogućiti određenu funkcionalnost sa slike
-
+//Testni primjer stringa 1.2
 
 public class Main {
     public static <Osoba> void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<Integer> listaBrojeva = new ArrayList<>();
-        List<String> popisImena = new ArrayList<>();
 
-        System.out.println("Unesite ime ili broj: ");
-        int broj = scanner.nextInt();
-        var ime = scanner.nextInt();
+        String nekiString = "Ovo je testni primjerak stringa 1.2";
+        Integer brojacSlova = 0;
 
-        for (int i = 0; i < broj; i++) {
-            System.out.println("Unesite željenu vrijednost: ");
-            listaBrojeva.add(scanner.nextInt());
-            popisImena.add(scanner.next());
-        }
+        for (int i = 0; i < nekiString.length(); i++) {
+            char character = nekiString.charAt(i);
+            boolean daLiJeSlovo = Character.isLetter(character);
+            boolean daLiJeBroj = Character.isDigit(character);
+            String stringCharacter = String.valueOf(character);
 
-        int trenutniBroj = listaBrojeva.get(0);
+            if (stringCharacter.equals(" ")) {
+                continue;
+            }
 
-        for (int i = 0; i < listaBrojeva.size(); i++){
+            if (daLiJeSlovo == true) {
+                System.out.println(character + " Slovo");
+                brojacSlova++;
+            } else if (daLiJeBroj == true) {
+                System.out.println(character + " Broj");
+            } else {
+                System.out.println(character + " Znak");
+            }
 
+
+            System.out.println(brojacSlova);
         }
     }
 }
