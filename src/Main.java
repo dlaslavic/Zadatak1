a330f06b46cf08cc08c81ef8b453ad9639921a21
dlaslavic <dejan.laslavic@gmail.com>
2024-10-30 21:06:08 +0100

    OOP u Javi

--- src/Main.java
+++ src/Main.java
@@ -6,75 +6,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-
-
 public class Main {
 
-    /* STATISTIKA RIJECI
-     * Napravi program koji učitava rečenicu i ispisuje:
-     * 1. Sve riječi duže od 5 slova
-     * 2. Najduzu rijec
-     * 3. Prosjecnu duljinu rijeci
-     */
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Unesi neku rečenicu: ");
-        String unesenaRecenica = scanner.nextLine();
+        Pas pasRex = new Pas("Rex", "Njemacki ovcar", 10);
+        Pas pasOskar = new Pas("Oskar", "Labrador", 5);
 
-        List<String> recenicaLista = new ArrayList<>();
+        String ime1 = pasRex.dohvatiIme();
+        String pasmina1 = pasRex.dohvatiPasminu();
+        String goidna1 = pasRex.dohvatiGodinuUStringu();
 
-        for (String rijec : unesenaRecenica.split(" ")) {
-            recenicaLista.add(rijec);
-        }
+        String ime2 = pasOskar.dohvatiIme();
+        String pasmina2 = pasOskar.dohvatiPasminu();
+        Integer goidna2 = pasOskar.dohvatiGodinu();
 
-        List<String> rijeciDuzeOdPet = rijeciDuzeOdPetSlova(recenicaLista);
-        String najduzaRijec = najduzaRijec(recenicaLista);
-        float prosjekDuljine = prosjecnaDuljinaRijeci(recenicaLista); //4.5
+        Integer staraGodina = pasOskar.dohvatiGodinu();
 
-        System.out.println("Riječi duže od 5 slova: " + rijeciDuzeOdPet);
-        System.out.println("Najduža riječ: " + najduzaRijec);
-        System.out.printf("Prosječna duljina riječi: " + prosjekDuljine);
-    }
+        pasOskar.promjeniGodinu(7);
 
-    private static List<String> rijeciDuzeOdPetSlova(List<String> rijeci) {
-        List<String> duzeOdPet = new ArrayList<>();
+        Integer novaGodina = pasOskar.dohvatiGodinu();
 
-//        for (int i = 0; i < rijeci.size(); i++){
-//            if (rijeci.get(i).length() > 5) {
-//                duzeOdPet.add(rijeci.get(i));
-//            }
-//        }
+        System.out.println(pasRex.dohvatiIme());
+        System.out.println(pasOskar.dohvatiIme());
 
-        for (String rijec : rijeci) {
-            if (rijec.length() > 5) {
-                duzeOdPet.add(rijec);
-            }
-        }
-        return duzeOdPet;
-    }
-
-    private static String najduzaRijec(List<String> rijeci) {
-        String najduza = "";
-        for (String rijec : rijeci) {
-            if (rijec.length() > najduza.length()) {
-                najduza = rijec;
-            }
-        }
-        return najduza;
-    }
-
-    private static float prosjecnaDuljinaRijeci(List<String> rijeci) {
-        float ukupnaDuljina =18;
-        for (String rijec : rijeci) {
-            ukupnaDuljina = ukupnaDuljina + rijec.length();
-        }
-        float rezultat = ukupnaDuljina / rijeci.size(); // 4.5
-
-        return rezultat; // 4.5
     }
 }
-
-
-
