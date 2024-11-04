c26e57ea32d37fa812744a0fdc611e27c290d342
dlaslavic <dejan.laslavic@gmail.com>
2024-11-04 20:31:14 +0100

    Pokusaj

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,33 @@
 
 
 import java.math.BigDecimal;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
 
-    public static void main(String[] args) {
+//Zadatak_1.
+//Kreirajte klasu Proizvod s privatnim atributima:
+//naziv (tipa String),
+//cijena (tipa BigDecimal)
+//popust( tipa BigDecimal)
+//Dodajte javni konstruktor za inicijalizaciju naziv i cijena
+//Kreirajte public metodu postaviPropust koja omogućava postavljanje popusta u postocima (tip BigDecimal)
+//Dodajte public metodu izracunajCijenu koja vraća konačnu cijenu proizvoda nakon što je popust primijenjen.
+//U glvanoj klasi kreirajte objekt klase Proizvod, postavite naziv, cijenu i popust te ispišite konačnu cijenu proizvoda.
 
-        Polaznik polaznik1 = new Polaznik("Jura", "Juric");
-        Polaznik polaznik2 = new Polaznik("Pero", "Peric", 45, "Musko");
-        Polaznik polaznik3 = new Polaznik("Miro", "Miric");
+    public class Main {
+        public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
 
+            Proizvod proizvod = new Proizvod("Mobitel", 200.00);
 
-
-        String ime = polaznik1.getIme();
-        String prezime = polaznik1.getPrezime();
-
-        Integer dob2 = polaznik2.getDob();
-        polaznik2.promijeniGodinu(42);
-        Integer novaDob2 = polaznik2.getDob();
-
-        String ime3 = polaznik3.getIme();
-        String prezime3 = polaznik3.getPrezime();
-
-        System.out.println(polaznik1.getIme());
-        System.out.println(polaznik1.getPrezime());
-
+        BigDecimal cijena = BigDecimal.valueOf(200.00);
+        BigDecimal popust = BigDecimal.valueOf(0.80);
+        BigDecimal konacnaCijena = cijena.multiply(popust);
 
     }
+
+}
+
 }
