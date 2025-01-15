70b87ef2ec799774f77e9da85cc6fe1c52685c15
dlaslavic <dejan.laslavic@gmail.com>
2025-01-15 20:20:20 +0100

    Database s predavanja

--- src/Main.java
+++ src/Main.java
@@ -1,70 +1,137 @@
-import java.util.HashSet;
-import java.util.TreeSet;
+import database.DatabaseService;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 
 public class Main {
     public static void main(String[] args) {
-        kreirajStudente();
-        kreirajGradove();
-        kreirajZaposlenike();
+        selectExample();
+        updateExample();
+        deleteExample();
+        insertExample();
+        
     }
 
-    static void  kreirajStudente() {
-        HashSet<String> studenti = new HashSet<>();
-        studenti.add("Marko");
-        studenti.add("Ivo");
-        studenti.add("Ana");
-        studenti.add("Borna");
+    private static void deleteExample() {
+        Connection connection = DatabaseService.createConnection();
 
-        if (studenti.contains("Borna")) {
-            System.out.println("Borna postoji");
-        } else {
-            System.out.println("Borna NE postoji");
-        }
-        System.out.println(studenti);
-        studenti.remove("Borna");
-        System.out.println(studenti);
-    }
+        try {
+            // Izvršavanje SQL upita
+            String query = "DELETE FROM Drzava WHERE IDDrzava = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
 
-    static void kreirajGradove() {
-        TreeSet<String> gradovi = new TreeSet<>();
-        gradovi.add("Zagreb");
-        gradovi.add("Rijeka");
-        gradovi.add("Osijek");
-        gradovi.add("Split");
+            statement.setString(1, "9");
 
-        System.out.println(gradovi);
-        System.out.println("Prvi grad je " + gradovi.first());
-        System.out.println("Zadnji grad je " + gradovi.last());
-        gradovi.remove("Rijeka");
-        System.out.println(gradovi);
-    }
+            Integer result = statement.executeUpdate();
 
-    static void kreirajZaposlenike() {
-        HashSet<String> zaposleniciA = new HashSet<>();
-        zaposleniciA.add("Marko");
-        zaposleniciA.add("Ivo");
-        zaposleniciA.add("Ana");
-        zaposleniciA.add("Borna");
-
-        HashSet<String> zaposleniciB = new HashSet<>();
-        zaposleniciB.add("Lara");
-        zaposleniciB.add("Jura");
-        zaposleniciB.add("Borna");
-        zaposleniciB.add("Dejan");
-
-        System.out.println(zaposleniciA);
-        System.out.println(zaposleniciB);
-
-        for (String zaposlenik : zaposleniciA) {
-            if (zaposleniciB.contains(zaposlenik)) {
-                System.out.println(zaposlenik + " radi u oba odjela");
+            // Ispis rezultata
+            System.out.println("Rezultat je " + result);
+            statement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
         }
-        for (String zaposlenik : zaposleniciA) {
-            if (!zaposleniciB.contains(zaposlenik)) {
-                System.out.println(zaposlenik + " radi SAMO u odjelu A");
+    }
+
+    private static void updateExample() {
+        Connection connection = DatabaseService.createConnection();
+
+        try {
+            // Izvršavanje SQL upita
+            String query = "UPDATE Drzava SET Naziv = ? WHERE IDDrzava = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+
+            statement.setString(1, "Portugal");
+            statement.setString(2, "9");
+
+            Integer result = statement.executeUpdate();
+
+            // Ispis rezultata
+            System.out.println("Rezultat je " + result);
+            statement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+
+    private static void insertExample() {
+        Connection connection = DatabaseService.createConnection();
+
+        try {
+            // Izvršavanje SQL upita
+            String query = "INSERT INTO Drzava (Naziv) VALUES (?)";
+            PreparedStatement statement = connection.prepareStatement(query);
+
+            statement.setString(1, "Slovenija");
+
+            Integer result = statement.executeUpdate();
+
+            // Ispis rezultata
+
+            System.out.println("Rezultat je " + result);
+            statement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+
+    private static void selectExample() {
+        Connection connection = DatabaseService.createConnection();
+
+        try {
+            // Izvršavanje SQL upita
+            String query = "SELECT * FROM Drzava ORDER BY Naziv";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            // Ispis rezultata
+            while (resultSet.next()) {
+                System.out.println("ID: " + resultSet.getInt("IDDrzava"));
+                System.out.println("Naziv: " + resultSet.getString("Naziv"));
+            }
+
+            // Zatvaranje resursa
+            resultSet.close();
+            statement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+
+
             }
         }
     }
